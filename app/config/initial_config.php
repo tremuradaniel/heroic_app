b7dcd1263033e69dc381d8ec01e2ba74625d586c
Daniel Tremura <tremuradaniel@gmail.com>
2020-11-04 08:16:32 +0200

    mvc structure

---
+++ app/config/initial_config.php
@@ -0,0 +1,13 @@
+<?php
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'your_root_url');
+    // Site name
+    define('SITENAME', 'your_sitename');
+
+    // DB params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'your_username');
+    define('DB_PASS', 'your_pass');
+    define('DB_NAME', 'your_dbName');
