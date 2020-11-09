f875a963ae6f0a257716c265ca72263e616f2510
Daniel Tremura <tremuradaniel@gmail.com>
2020-11-09 09:26:52 +0200

    updating get traits interval func

--- app/helpers/combatant.helper.php
+++ app/helpers/combatant.helper.php
@@ -2,15 +2,22 @@
 
     
     /**
-     * getCombatantSkillsInteravalQuerry
+     * getCombatantTraitsInteravalQuerry
      *
      * @param  string $type
      * @return string
      */
-    function getCombatantSkillsInteravalQuerry (String $type) {
-        return "SELECT * FROM combatant_skills_intervals
-            INNER JOIN combatant_types
-            ON combatant_skills_intervals.combatant_type_id = combatant_types.id
-            WHERE combatant_types.type = '{$type}'
-            ORDER BY combatant_skills_intervals.trait_id;";
+    function getCombatantTraitsInteravalQuerry (String $type) {
+        return 
+            "SELECT 
+                combatant_traits.trait,
+                combatant_traits_intervals.min,
+                combatant_traits_intervals.max
+            FROM combatant_traits_intervals
+                INNER JOIN combatant_types
+            ON combatant_traits_intervals.combatant_type_id = combatant_types.id
+                INNER JOIN combatant_traits 
+            ON combatant_traits_intervals.trait_id = combatant_traits.id
+                WHERE combatant_types.`type` = '{$type}'
+            ORDER BY combatant_traits_intervals.trait_id;";
     }
